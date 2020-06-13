40e270b997dd1b9f462af31a53594af0ff7624a2
xakml <xakml@qq.com>
2020-06-13 17:41:56 +0800

    1. 增加MySQL库user表的建表脚本; 2. 新增最简单的入门测试示例SmartSQL.SimpleTest

---
+++ src/Test/DbScripts/MySql/T_user_ddl.sql
@@ -0,0 +1,8 @@
+﻿-- 测试环境
+-- MySQL 5.7.21
+CREATE TABLE `smartsqltestdb`.`t_user` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '长整型主键,自增',
+  `UserName` VARCHAR(50) NULL,
+  `Status` SMALLINT NULL,
+  PRIMARY KEY (`id`))
+COMMENT = 'SmartSql测试用表';
---
+++ src/Test/SmartSql.SimpleTest/Entities/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SmartSql.SimpleTest.Entities
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public User(long id)
+        {
+            Id = id;
+        }
+
+        public User(long id, string name)
+        {
+            Id = id;
+            User_Name = name;
+        }
+
+        public User(long id, string name, UserStatus status)
+        {
+            Id = id;
+            User_Name = name;
+            Status = status;
+        }
+
+        public virtual long Id { get; set; }
+        public virtual String User_Name { get; set; }
+        public virtual UserStatus Status { get; set; }
+    }
+
+    public enum UserStatus : Int16
+    {
+        Ok = 1
+    }
+}
