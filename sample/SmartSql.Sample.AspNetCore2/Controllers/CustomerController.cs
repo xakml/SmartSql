88452b08da5f6eb2ba102908d27a4fb21158e657
xakml <xakml@qq.com>
2020-08-03 16:08:12 +0800

    项目： SmartSql.Sample.AspNetCore2 直接使用nuget引用已发布的库，已测试通过

---
+++ sample/SmartSql.Sample.AspNetCore2/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartSql.Sample.AspNetCore2.DyRepositories;
+
+namespace SmartSql.Sample.AspNetCore2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public CustomerController(ICustomerRepository userRepository)
+        {
+            _customerRepository = userRepository;
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Entities.Customer cust)
+        {
+            long id = this._customerRepository.Insert(cust);
+            return Ok(id);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var list = this._customerRepository.SelectAll();
+            return Ok(list);
+        }
+
+        [HttpGet("{id:long}")]
+        public IActionResult GetById(long id)
+        {
+            var list = this._customerRepository.SelectByCondition(new DTO.CustomerQueryConditon() { Id = id });
+            return Ok(list);
+        }
+
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var list = this._customerRepository.SelectByCondition(new DTO.CustomerQueryConditon() { Name = name });
+            return Ok(list);
+        }
+    }
+}
