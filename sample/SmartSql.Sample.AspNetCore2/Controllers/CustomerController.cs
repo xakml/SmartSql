6563a91b9906f29dbd942bd4f08b791124113e8c
xakml <xakml@qq.com>
2020-08-03 19:21:48 +0800

    使用nuget方案引用dll，创建项目

--- sample/SmartSql.Sample.AspNetCore2/Controllers/CustomerController.cs
+++ sample/SmartSql.Sample.AspNetCore2/Controllers/CustomerController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SmartSql.Sample.AspNetCore2.DyRepositories;
 
 namespace SmartSql.Sample.AspNetCore2.Controllers
